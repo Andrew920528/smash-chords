40028dfd881e2837439b5f5fab06969b672eee59
Andrew920528 <andrewhsu.0528@gmail.com>
2026-02-19 02:59:48 -0500

    theme preview

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,13 +1,8 @@
 import { ThemePreview } from "./components/ThemePreview";
-import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
-      <Button>Default</Button>
-      <Button variant="destructive">Delete</Button>
-      <Button size="lg">Large Button</Button>
-      <div className="yo bg-primary">hello</div>
       <ThemePreview />
     </>
   );
